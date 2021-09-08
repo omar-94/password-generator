3185bc47c1b4f20856d15a88c3cf31f5763bac99
onava <enava23@yahoo.com>
2021-09-08 00:05:42 -0400

    if chooseLength statement

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,17 @@ var numberChar = '0123456789';
 var specialChar = '!"#$%&()*+,-./:;<=>?@[]^_`{|}~';
 
 
+
 function generatePassword() {
-  window.prompt("Choose a password length between 8 and 128.", "8-128")
+  var chooseLength = window.prompt("Choose a password length between 8 and 128.", "8-128")
+
+  if (chooseLength <= 7 || chooseLength >= 129) {
+    alert("Password length must range between 8-128 characters. Please choose a valid number.");
+    var chooseLength = window.prompt("Choose a password length between 8 and 128.", "8-128")
+  } else {
+    passwordLength = chooseLength
+    console.log(chooseLength)
+  }
 }
 
 // Write password to the #password input

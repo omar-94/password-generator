58ae447690b1972c1e35018de7ef0b3629f10ea7
onava <enava23@yahoo.com>
2021-09-07 23:41:04 -0400

    variables for characters to generate from

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Characters to generate from
+var lowercaseChar = 'abcdefghijklmnopqrstuvwxyz';
+var uppercaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numberChar = '0123456789';
+var specialChar = '!"#$%&()*+,-./:;<=>?@[]^_`{|}~';
+
 
 function generatePassword() {
   window.prompt("Choose a password length between 8 and 128.", "8-128")

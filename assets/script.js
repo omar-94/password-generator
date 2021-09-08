6361c27eca22e10053f21e966879339e9f9d19ea
onava <enava23@yahoo.com>
2021-09-08 00:37:14 -0400

    var chooseCharType  confirms / edit var chooseLength

--- assets/script.js
+++ assets/script.js
@@ -11,13 +11,31 @@ var specialChar = '!"#$%&()*+,-./:;<=>?@[]^_`{|}~';
 
 function generatePassword() {
   var chooseLength = window.prompt("Choose a password length between 8 and 128.")
+  console.log(chooseLength)
 
   while (chooseLength <= 7 || chooseLength >= 129) {
     alert("Password length must range between 8-128 characters. Please choose a valid number.");
     chooseLength = window.prompt("Choose a password length between 8 and 128.");
-    passwordLength = chooseLength;
-    console.log(passwordLength);
+    console.log(chooseLength);
   }
+
+  var chooseLowercaseChar = confirm("Select OK to include lowercase characters.");
+  var chooseUppercaseChar = confirm("Select OK to include uppercase characters.");
+  var chooseNumberChar = confirm("Select OK to include numerical characters.");
+  var chooseSpecialChar = confirm("Select OK to include special characters.");
+
+  while (!chooseLowercaseChar && !chooseUppercaseChar && !chooseNumberChar && !chooseSpecialChar) {
+    alert("Please choose at least one character type to include.");
+    chooseLowercaseChar = confirm("Select OK to include lowercase characters.");
+    chooseUppercaseChar = confirm("Select OK to include uppercase characters.");
+    chooseNumberChar = confirm("Select OK to include numerical characters.");
+    chooseSpecialChar = confirm("Select OK to include special characters.");
+  }
+
+  
+
+
+
 }
 
 // Write password to the #password input
